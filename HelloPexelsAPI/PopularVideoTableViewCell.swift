09ece11ea20566a903b7270b88c907546a4e47d6
Willy <willy548798@gmail.com>
2025-02-22 17:25:58 +0800

    update url connect

---
+++ HelloPexelsAPI/PopularVideoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PopularVideoTableViewCell.swift
+//  HelloPexelsAPI
+//
+//  Created by Willy Hsu on 2025/2/22.
+//
+
+import UIKit
+
+class PopularVideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
